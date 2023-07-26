8a5976023880c8b514ace0c3036cbb42b2e92fe4
Nihal00 <83398133+Nihal00@users.noreply.github.com>
2023-07-26 08:31:31 +0000

    one

--- src/components/App.js
+++ src/components/App.js
@@ -2,10 +2,34 @@
 import React from "react";
 import './../styles/App.css';
 
+let fruits = ["apple", "banana", "cherry", "date", "elderberry", "fig"]
+
+
 const App = () => {
+
+  let [item, setItem] = useState(fruits);
+
+  function update(e) {
+    let letter = e.target.value;
+
+    if(letter === ""){
+      setItem(fruits);
+    } else {
+      let newArr = fruits.filter(ele => ele.includes(letter));
+    }
+  }
+
   return (
     <div>
         {/* Do not remove the main div */}
+        <h1>Search Item</h1>
+        <input type="text" onChange={ update } />
+
+        <ul>
+          {
+            item.map((it) => <li>{it}</li>)
+          }
+        </ul>
     </div>
   )
 }
